f4a406891b121db5893ffdd3344d528cc388ff19
Betty310-frontend <hg.betty310@gmail.com>
2026-04-24 22:23:18 +0900

    fix: makeTriangle 코드 정리

--- 6.JS/makeTriangle.js
+++ 6.JS/makeTriangle.js
@@ -1,13 +1,51 @@
 // 1. 왼쪽 삼각형
 function leftTriangle(n) {
-    for(let i=1; i <= n; i++){
-        console.log('*'.repeat(i))
-    }
+  for (let i = 1; i <= n; i++) {
+    console.log("*".repeat(i));
+  }
 }
-console.log('1. 왼쪽 삼각형');
-leftTriangle(5);
-console.log()
-/*
+
+// 2. 왼쪽 역삼각형
+function leftInvertedTriangle(n) {
+  for (let i = n; i > 0; i--) {
+    console.log("*".repeat(i));
+  }
+}
+
+// 3. 오른쪽 삼각형
+function rightTriangle(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "*".repeat(i));
+  }
+}
+
+// 4. 오른쪽 역삼각형
+function rightInvertedTriangle(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(" ".repeat(n - i) + "*".repeat(i));
+  }
+}
+
+// 5. 이등변 삼각형
+function isoscelesTriangle(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+}
+
+// 6. 마름모
+function rhombus(n) {
+  isoscelesTriangle(n);
+  for (let j = n - 1; j >= 1; j--) {
+    console.log(" ".repeat(n - j) + "*".repeat(2 * j - 1));
+  }
+}
+
+function main() {
+  console.log("1. 왼쪽 삼각형");
+  leftTriangle(5);
+  console.log();
+  /*
 출력 결과:
 
 *
@@ -18,16 +56,10 @@ console.log()
 
 */
 
-// 2. 왼쪽 역삼각형
-function leftInvertedTriangle(n) {
-    for (let i = n; i > 0; i--){
-        console.log('*'.repeat(i));
-    }
-}
-console.log('2. 왼쪽 역삼각형');
-leftInvertedTriangle(5);
-console.log()
-/*
+  console.log("2. 왼쪽 역삼각형");
+  leftInvertedTriangle(5);
+  console.log();
+  /*
 출력 결과:
 
 *****
@@ -38,17 +70,10 @@ console.log()
 
 */
 
-// 3. 오른쪽 삼각형
-function rightTriangle(n) {
-    for(let i=1; i <= n; i++){
-        console.log(' '.repeat(n-i) + '*'.repeat(i));
-    }
-}
-
-console.log('3. 오른쪽 삼각형');
-rightTriangle(5);
-console.log()
-/*
+  console.log("3. 오른쪽 삼각형");
+  rightTriangle(5);
+  console.log();
+  /*
 출력 결과:
 
     *
@@ -59,17 +84,10 @@ console.log()
 
 */
 
-// 4. 오른쪽 역삼각형
-function rightInvertedTriangle(n) {
-    for(let i=n; i > 0; i--){
-        console.log(' '.repeat(n-i) + '*'.repeat(i));
-    }
-}
-
-console.log('4. 오른쪽 역삼각형');
-rightInvertedTriangle(5);
-console.log();
-/*
+  console.log("4. 오른쪽 역삼각형");
+  rightInvertedTriangle(5);
+  console.log();
+  /*
 출력 결과:
 
 *****
@@ -80,46 +98,36 @@ console.log();
 
 */
 
-// 5. 이등변 삼각형
-function isoscelesTriangle(n){
-    const half = Math.round(n / 2);
-    for(let i=1; i <= half; i++) {
-        console.log(' '.repeat(half-i) + '*'.repeat((2*i)-1) + ' '.repeat(half-i));
-    }
-}
-
-console.log('5. 이등변 삼각형');
-isoscelesTriangle(5);
-console.log();
-/*
+  console.log("5. 이등변 삼각형");
+  isoscelesTriangle(5);
+  console.log();
+  /*
 출력 결과:
 
-  *  
- *** 
-*****
+    *
+   ***
+  *****
+ *******
+*********
 
 */
 
-// 6. 마름모
-function rhombus(n){
-    const half = Math.round(n / 2);
-    for(let i=1; i <= half; i++) {
-        console.log(' '.repeat(half-i) + '*'.repeat((2*i)-1) + ' '.repeat(half-i));
-    }
-    for (let j=half-1; j >= 1; j--) {
-        console.log(' '.repeat(half-j) + '*'.repeat((2*j)-1) + ' '.repeat(half-j));
-    }
-}
-
-console.log('6. 마름모');
-rhombus(5)
-/*
+  console.log("6. 마름모");
+  rhombus(5);
+  /*
 출력 결과:
 
-  *  
- *** 
-*****
- *** 
-  * 
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
 
  */
+}
+
+main();
